91c044ba85bd181eddb7a6332179f27a1991ea33
Double0negative <soccerkid9094@gmail.com>
2021-12-20 16:23:14 -0500

    update urls

--- frontend/src/plugins/apex.js
+++ frontend/src/plugins/apex.js
@@ -9,23 +9,23 @@ export default {
 function apexService(config) {
 
     async function getStats(eventId, round) {
-        let stats = await axios.get(config.baseUrl + "/stats/event/" + eventId + "/round/" + round);
+        let stats = await axios.get(config.baseUrl + "stats/event/" + eventId + "/round/" + round);
         return stats.data;
     }
 
     async function generateStats(eventId, statsCode, round, skipFetch) {
-        await axios.post(config.baseUrl + "/stats", {
+        await axios.post(config.baseUrl + "stats", {
             eventId, statsCode, round, skipFetch,
         })
     }
 
     async function getDisplayView(eventId) {
-        let data = await axios.get(config.baseUrl + "/display/" + eventId);
+        let data = await axios.get(config.baseUrl + "display/" + eventId);
         return data.data;
     }
 
     async function setDisplayView(eventId, display) {
-        await axios.post(config.baseUrl + "/display/" + eventId, display);
+        await axios.post(config.baseUrl + "display/" + eventId, display);
     } 
 
     return {
